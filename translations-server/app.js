605f973e39a206dba594a0024716b55e17fb05c0
adiyathereh <ad.kydy@gmail.com>
2018-09-03 16:18:01 +0200

    Create Sign up: fixed redirect

--- translations-server/app.js
+++ translations-server/app.js
@@ -53,40 +53,52 @@ app.set("views", path.join(__dirname, "../translations-client/views"));
 app.set("view engine", "hbs");
 
 //PASSPORT SETUP
-// require("./handlers/auth");
 passport.serializeUser((user, cb) => {
-  cb(null, user.id);
+  console.log(user.id, user.collection.collectionName);
+  cb(null, { id: user.id, role: user.collection.collectionName });
 });
 
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
-});
-
-passport.use(
-  "local-login",
-  new LocalStrategy((username, password, next) => {
-    //TODO ADD AUTH WITH EMAIL
-
-    User.findOne({ username }, (err, user) => {
+passport.deserializeUser((user, cb) => {
+  console.log("ID: ", user);
+  if (user.role === "translators") {
+    Translator.findById(user.id, (err, user) => {
       if (err) {
-        return next(err);
+        return cb(err);
       }
-      if (!user) {
-        return next(null, false, { message: "Incorrect username" });
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return next(null, false, { message: "Incorrect password" });
-      }
-
-      return next(null, user);
+      cb(null, user);
     });
-  })
-);
+  } else if (user.role === "wos") {
+    WO.findById(user.id, (err, user) => {
+      if (err) {
+        return cb(err);
+      }
+      console.log("Hallo");
+      cb(null, user);
+    });
+  }
+});
+
+// passport.use(
+//   "local-login",
+//   new LocalStrategy((username, password, next) => {
+//     //TODO ADD AUTH WITH EMAIL
+//     console.log("signin");
+
+//     User.findOne({ username }, (err, user) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       if (!user) {
+//         return next(null, false, { message: "Incorrect username" });
+//       }
+//       if (!bcrypt.compareSync(password, user.password)) {
+//         return next(null, false, { message: "Incorrect password" });
+//       }
+
+//       return next(null, user);
+//     });
+//   })
+// );
 
 passport.use(
   "local-signup",
@@ -95,11 +107,11 @@ passport.use(
     (req, username, password, next) => {
       // To avoid race conditions
       const { email, role, idNumber } = req.body;
-      console.log(role);
       process.nextTick(() => {
         const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-        //if there is a role in the req.body this request comes from the translator sign up
-        if (role === "translator") {
+        //check for roles: either WO or translator
+
+        if (role === "Volunteer" || role === "Professional") {
           Translator.findOne(
             {
               username
@@ -111,30 +123,23 @@ passport.use(
               if (user) {
                 return next(null, false);
               } else {
-                // Destructure the body
-                console.log("i am in translator", req.body);
-                // const { username, email, password, role } = req.body;
-                // const hashPass = bcrypt.hashSync(
-                //   password,
-                //   bcrypt.genSaltSync(8),
-                //   null
-                // );
-
-                new Translator({
+                const newTranslator = new Translator({
                   username,
                   email,
                   password: hashPass,
                   role
-                })
-                  .save()
-                  .then(result => {
-                    console.log(result);
-                  });
+                });
+
+                newTranslator.save(err => {
+                  if (err) {
+                    next(null, false, { message: newTranslator.errors });
+                  }
+                  return next(null, newTranslator);
+                });
               }
             }
           );
         } else if (role === "wo") {
-          console.log("i am in wo");
           WO.findOne(
             {
               username
@@ -146,19 +151,19 @@ passport.use(
               if (user) {
                 return next(null, false);
               } else {
-                // Destructure the body
-                console.log("req body:", req.body);
-
-                new WO({
+                const newWo = new WO({
                   username,
                   email,
                   password: hashPass,
                   idNumber
-                })
-                  .save()
-                  .then(result => {
-                    console.log(result);
-                  });
+                });
+
+                newWo.save(err => {
+                  if (err) {
+                    next(null, false, { message: newWo.errors });
+                  }
+                  return next(null, newWo);
+                });
               }
             }
           );
--- translations-server/routes/auth.js
+++ translations-server/routes/auth.js
@@ -6,22 +6,31 @@ const fs = require("fs");
 // const User = require("../models/user");
 const fileUpload = require("express-fileupload");
 
-router.get("/login", ensureLoggedOut(), (req, res) => {
-  res.render("auth/login", { message: req.flash("error") });
-});
+// router.get("/login", ensureLoggedOut(), (req, res) => {
+//   res.render("auth/login", { message: req.flash("error") });
+// });
+
+// router.post(
+//   "/login",
+//   ensureLoggedOut(),
+//   passport.authenticate("local-login", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//     failureFlash: true
+//   })
+// );
 
 router.post(
-  "/login",
-  ensureLoggedOut(),
-  passport.authenticate("local-login", {
+  "/signup",
+  passport.authenticate("local-signup", {
     successRedirect: "/",
-    failureRedirect: "/login",
+    failureRedirect: "/blah",
     failureFlash: true
   })
 );
 
 router.get("/signup/:role", (req, res) => {
-  console.log(req.params);
+  // console.log(req.params);
   if (req.params.role === "wo") {
     res.render("auth/signup", {
       // message: req.flash("error"),
@@ -35,18 +44,9 @@ router.get("/signup/:role", (req, res) => {
   }
 });
 
-router.post(
-  "/signup/:role",
-  passport.authenticate("local-signup", {
-    successRedirect: "/login",
-    failureRedirect: "/signup",
-    failureFlash: true
-  })
-);
-
-router.get("/logout", ensureLoggedIn("/login"), (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
+// router.get("/logout", ensureLoggedIn("/login"), (req, res) => {
+//   req.logout();
+//   res.redirect("/");
+// });
 
 module.exports = router;

093c984eb6b0fc83c130bf8424822ddca3c68ac3
Astrid Varga <astrid.varga@gmx.de>
2018-09-03 18:47:15 +0200

    create meeting code: modify get and post routes to check for 'logged in as WO' in order to get id of logged in WO - stopped to wait for login bugfix

--- translations-server/app.js
+++ translations-server/app.js
@@ -16,7 +16,6 @@ const meetingRouter = require("./routes/meeting");
 const LocalStrategy = require("passport-local").Strategy;
 const Translator = require("./models/Translator");
 const WO = require("./models/WO");
-const Meeting = require("./models/Meeting");
 const bcrypt = require("bcrypt");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
@@ -26,8 +25,8 @@ const cookieParser = require("cookie-parser");
 //MONGO SETUP
 //connect to MongoDB
 mongoose.connect(
-  "mongodb://localhost/translations",
-  { useNewUrlParser: true }
+    "mongodb://localhost/translations",
+    { useNewUrlParser: true }
 );
 
 //serves all files from translations-client/public folder through "/"
@@ -36,12 +35,12 @@ app.use(express.static(path.join(__dirname, "../translations-client/public")));
 //Save sessions so that there is no need
 //to constantly log in when server is restarted
 app.use(
-  session({
-    secret: "translations",
-    resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-  })
+    session({
+        secret: "translations",
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
 );
 
 app.use(flash());
@@ -56,25 +55,25 @@ app.set("view engine", "hbs");
 
 //PASSPORT SETUP
 passport.serializeUser((user, cb) => {
-  cb(null, { id: user.id, role: user.collection.collectionName });
+    cb(null, { id: user.id, role: user.collection.collectionName });
 });
 
 passport.deserializeUser((user, cb) => {
-  if (user.role === "translators") {
-    Translator.findById(user.id, (err, user) => {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, user);
-    });
-  } else if (user.role === "wos") {
-    WO.findById(user.id, (err, user) => {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, user);
-    });
-  }
+    if (user.role === "translators") {
+        Translator.findById(user.id, (err, user) => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, user);
+        });
+    } else if (user.role === "wos") {
+        WO.findById(user.id, (err, user) => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, user);
+        });
+    }
 });
 
 // passport.use(
@@ -100,76 +99,73 @@ passport.deserializeUser((user, cb) => {
 // );
 
 passport.use(
-  "local-signup",
-  new LocalStrategy(
-    { passReqToCallback: true },
-    (req, username, password, next) => {
-      // To avoid race conditions
-      const { email, role, idNumber } = req.body;
-      process.nextTick(() => {
-        const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-        //check for roles: either WO or translator
+    "local-signup",
+    new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
+        // To avoid race conditions
+        const { email, role, idNumber } = req.body;
+        process.nextTick(() => {
+            const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+            //check for roles: either WO or translator
 
-        if (role === "Volunteer" || role === "Professional") {
-          Translator.findOne(
-            {
-              username
-            },
-            (err, user) => {
-              if (err) {
-                return next(err);
-              }
-              if (user) {
-                return next(null, false);
-              } else {
-                const newTranslator = new Translator({
-                  username,
-                  email,
-                  password: hashPass,
-                  role
-                });
+            if (role === "Volunteer" || role === "Professional") {
+                Translator.findOne(
+                    {
+                        username
+                    },
+                    (err, user) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        if (user) {
+                            return next(null, false);
+                        } else {
+                            const newTranslator = new Translator({
+                                username,
+                                email,
+                                password: hashPass,
+                                role
+                            });
 
-                newTranslator.save(err => {
-                  if (err) {
-                    next(null, false, { message: newTranslator.errors });
-                  }
-                  return next(null, newTranslator);
-                });
-              }
+                            newTranslator.save(err => {
+                                if (err) {
+                                    next(null, false, { message: newTranslator.errors });
+                                }
+                                return next(null, newTranslator);
+                            });
+                        }
+                    }
+                );
+            } else if (role === "wo") {
+                WO.findOne(
+                    {
+                        username
+                    },
+                    (err, user) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        if (user) {
+                            return next(null, false);
+                        } else {
+                            const newWo = new WO({
+                                username,
+                                email,
+                                password: hashPass,
+                                idNumber
+                            });
+
+                            newWo.save(err => {
+                                if (err) {
+                                    next(null, false, { message: newWo.errors });
+                                }
+                                return next(null, newWo);
+                            });
+                        }
+                    }
+                );
             }
-          );
-        } else if (role === "wo") {
-          WO.findOne(
-            {
-              username
-            },
-            (err, user) => {
-              if (err) {
-                return next(err);
-              }
-              if (user) {
-                return next(null, false);
-              } else {
-                const newWo = new WO({
-                  username,
-                  email,
-                  password: hashPass,
-                  idNumber
-                });
-
-                newWo.save(err => {
-                  if (err) {
-                    next(null, false, { message: newWo.errors });
-                  }
-                  return next(null, newWo);
-                });
-              }
-            }
-          );
-        }
-      });
-    }
-  )
+        });
+    })
 );
 
 app.use(passport.initialize());
@@ -183,14 +179,14 @@ app.use("/", meetingRouter);
 
 //ERRORS SETUP
 app.use((req, res, next) => {
-  let err = new Error("Not found");
-  err.status = 404;
-  next(err);
+    let err = new Error("Not found");
+    err.status = 404;
+    next(err);
 });
 app.use(errorHandler);
 
 app.listen(config.PORT, () => {
-  console.log(`server starting on port ${config.PORT}`);
+    console.log(`server starting on port ${config.PORT}`);
 });
 
 module.exports = app;
--- translations-server/routes/meeting.js
+++ translations-server/routes/meeting.js
@@ -1,40 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const Translator = require("../models/Translator");
+const Meeting = require("../models/Meeting");
 
-router.get("/meeting-create", (req, res) => {
-    res.render("meeting-create");
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+
+// app.get("/settings", ensureLoggedIn("/login"), function(req, res) {
+//     res.render("settings", { user: req.user });
+// });
+
+
+// TODO Test, if meeting-create works if logged in as WO
+
+router.get("/meeting-create", ensureLoggedIn("/login"), function(req, res) {
+    console.log("Req User " + req.user);
+    if (!req.user.role) {
+        // WO has no role, so if there's none, it has to be WO!
+        res.render("meeting-create");
+    } else {
+        res.render("auth/login");
+    }
 });
 
-// router.get("/update-book/:id", (req, res) => {
-//     const { id } = req.params;
-//     Book.findById(id).then(book => {
-//         res.render("update-book", { book });
-//     });
-// });
-
-// router.get("/book/:id", (req, res) => {
-//     Book.findById(req.params.id)
-//         .then(book => {
-//             res.render("book-details", { book });
-//         })
-//         .catch(console.error);
-// });
+// TODO test params that route gets, works!
 
 router.post("/meeting-create/:id", (req, res) => {
     const { id } = req.params;
+    console.log("WO ID " + id);
     const { date, time, translator_mail } = req.body;
-    const translatorID = Translator.find({ email: translator_mail }).then(translator => {});
-    console.log(translatorID);
-    new Meeting({
-        date,
-        time,
-        wo: id
-    })
-        .save()
-        .then(meeting => {
-            res.render("meeting", { meeting, new: true });
-        });
+    let translatorId;
+    Translator.find({ email: translator_mail }).then(translator => {
+        translatorId = translator[0]._id;
+
+        new Meeting({
+            date,
+            time,
+            wo: id,
+            translator: translatorId
+        })
+            .save()
+            .then(meeting => {
+                res.render("meeting", { meeting, new: true });
+            });
+    });
 });
 
 module.exports = router;
